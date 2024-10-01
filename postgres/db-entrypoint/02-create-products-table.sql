9b3f3450c996c187e777467cf1d4bfbe3075fc69
delissgt <delissgt@gmail.com>
2024-10-01 16:55:51 -0600

    Update Products Table

--- postgres/db-entrypoint/02-create-products-table.sql
+++ postgres/db-entrypoint/02-create-products-table.sql
@@ -2,8 +2,9 @@ CREATE TABLE products
 (
     "id"                uuid    NOT NULL DEFAULT uuid_generate_v4(),
     "name"              varchar            DEFAULT ''::character varying,
+    "ean"               int                DEFAULT 0,
     "brand"             varchar            DEFAULT ''::character varying,
-    "full_price"        varchar            DEFAULT ''::character varying,
+    "full_price"        float              DEFAULT 0.0,
     "discount_price"    float              DEFAULT 0.0,
     "in_stock"          bool               DEFAULT false,
     "url"               varchar            DEFAULT ''::character varying,

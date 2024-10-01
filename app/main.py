38b698fc306569f59f4f4f0f4561c03025d67671
delissgt <delissgt@gmail.com>
2024-10-01 17:21:37 -0600

    Obtain Product Descriptions and Save them in the Database

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import re
-
+from selenium.common.exceptions import NoSuchElementException
+from db import insert_product
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,7 +12,7 @@ driver.get('https://tienda.makro.com.co/ca/despensa/aceites/oliva/CP_12/CP_12_01
 store_url_product = "https://tienda.makro.com.co/p/"
 pattern_ean = r'(\d{6})'
 
-sleep(40)
+sleep(70)
 
 
 my_elements = driver.find_elements(by=By.CSS_SELECTOR, value=".card-product-vertical.product-card-default")
@@ -19,24 +20,26 @@ my_elements = driver.find_elements(by=By.CSS_SELECTOR, value=".card-product-vert
 
 for element in my_elements:
 
-    product_name = element.find_element(by=By.CSS_SELECTOR, value=".CardName__CardNameStyles-sc-147zxke-0.bWeSzf.prod__name")
-    print("product_name::::::", product_name.text)
+    try:
 
-    product_price = element.find_element(by=By.CSS_SELECTOR, value=".CardBasePrice__CardBasePriceStyles-sc-1dlx87w-0.bhSKFL.base__price")
-    print("product_price::::::", product_price.text)
+        product_name = element.find_element(by=By.CSS_SELECTOR, value=".CardName__CardNameStyles-sc-147zxke-0.bWeSzf.prod__name").text
 
-    product_url_img = element.find_element(by=By.TAG_NAME, value="img").get_attribute("src")
-    print("product_url_img::::::", product_url_img)
+        full_price = element.find_element(by=By.CSS_SELECTOR, value=".CardBasePrice__CardBasePriceStyles-sc-1dlx87w-0.bhSKFL.base__price")
+        full_price = float(re.sub(r'[^\w]', '', full_price.text))
 
-    product_ean: int = 000000
-    product_url: str = ""
+        discount_price = element.find_element(by=By.CLASS_NAME, value="base__price")
+        discount_price = float(re.sub(r'[^\w]', '', discount_price.text))
 
-    if product_url_img != "https://makro.com.co/imagesProducts/medias/134156_500X500_ON_100000000.png?":
-        product_ean = int(re.findall(pattern_ean, product_url_img)[0])
-        product_url = store_url_product + str(product_ean)
+        product_url_img = element.find_element(by=By.TAG_NAME, value="img").get_attribute("src")
 
-    print("product_EAN::::::", product_ean)
-    print("product_url::::::", product_url)
+        product_ean: int = 000000
+        product_url: str = ""
 
-    print("----------------------")
+        if product_url_img != "https://makro.com.co/imagesProducts/medias/134156_500X500_ON_100000000.png?":
+            product_ean = int(re.findall(pattern_ean, product_url_img)[0])
+            product_url = store_url_product + str(product_ean)
+
+        insert_product(product_name, product_ean, full_price, discount_price, product_url, product_url_img)
+    except NoSuchElementException:
+        pass
 
